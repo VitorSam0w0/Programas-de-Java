6c6fb86da6e095a6d1bd1396dd58007a40fa736b
Vitor Sam <temadark2022@gmail.com>
2022-08-27 19:03:27 -0300

    Update
    Atualização da pasta geral

---
+++ AtividadesparaTreino/Imc/imc.java
@@ -0,0 +1,17 @@
+package AtividadesparaTreino.Imc;
+
+import AtividadesparaTreino.Imc.Scanner.scanner
+
+import AtividadesparaTreino.Imc.Scanner.scanner;
+
+public class imc {
+    public static void main(String[] args) {
+
+        scanner oscanner = new scanner();
+
+        System.out.println(oscanner);
+
+
+
+    }
+}
