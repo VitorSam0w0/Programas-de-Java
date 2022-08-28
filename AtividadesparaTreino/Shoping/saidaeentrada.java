65a223c192a04dafc1e1e052bb015f3f1646faf1
Vitor Sam <temadark2022@gmail.com>
2022-08-27 21:59:36 -0300

    Novo Programa
    Numero de pessoas no shoping , treinando orientação a objeto...

---
+++ AtividadesparaTreino/Shoping/saidaeentrada.java
@@ -0,0 +1,17 @@
+package AtividadesparaTreino.Shoping;
+
+public class saidaeentrada {
+    
+
+    int saida;
+    int entrada;
+    int valorfinal;
+
+    void sairao (int sairaodoshop) {
+        valorfinal-=sairaodoshop;
+    }
+
+    void entrarao (int entraraonoshop) {
+        valorfinal+=entraraonoshop;
+    }
+}
