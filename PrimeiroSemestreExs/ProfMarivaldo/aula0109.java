96bfd483f39e1c66a6aa5a5f2f2eda4d11a8338c
Vitor Sam <temadark2022@gmail.com>
2022-09-03 11:59:54 -0300

    Create aula0109.java

---
+++ PrimeiroSemestreExs/ProfMarivaldo/aula0109.java
@@ -0,0 +1,20 @@
+package PrimeiroSemestreExs.ProfMarivaldo;
+
+public class aula0109 {
+    
+    public static void main(Script[] args) {
+
+        System.out.println("Sistema Logico");
+
+        int a = 10;
+        int b = 20;
+
+        if (a>b) {
+            System.out.println("a");
+        } else {
+            System.out.println("b");
+        }
+    }
+
+
+}
